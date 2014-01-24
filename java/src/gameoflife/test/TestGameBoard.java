f532fd61942e45e43e65e09b509f994f71260619
Lennart Hensler <LennartHensler@aol.com>
2014-01-24 10:14:52 +0100

    Added hamcrest and mockito jars

---
+++ java/src/gameoflife/test/TestGameBoard.java
@@ -0,0 +1,14 @@
+package gameoflife.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGameBoard {
+
+	@Test
+	public void testBoardInitialization() {
+		GameBoard board = new ToroidGameBoard(50, 50, )
+	}
+
+}
