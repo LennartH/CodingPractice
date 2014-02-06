39c3cad2b5349d9cdb94a1ec4aaaba27163682d7
Lennart Hensler <LennartHensler@aol.com>
2014-02-06 13:26:34 +0100

    Improved the DoubleTextField

--- java/GameOfLife.JavaFrontend/src/gameoflife/javafrontend/impl/component/DoubleTextField.java
+++ java/GameOfLife.JavaFrontend/src/gameoflife/javafrontend/impl/component/DoubleTextField.java
@@ -3,26 +3,40 @@ package gameoflife.javafrontend.impl.component;
 import gameoflife.javafrontend.impl.AbstractProvidesComponent;
 
 import java.awt.Component;
-import java.text.NumberFormat;
 
-import javax.swing.JFormattedTextField;
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
 
 public class DoubleTextField extends AbstractProvidesComponent {
     
-    private final JFormattedTextField textField;
+    private final JTextField textField;
 
     public DoubleTextField(double initialValue, int columns) {
-        NumberFormat format = NumberFormat.getNumberInstance();
-        format.setMaximumFractionDigits(1);
-        format.setMinimumFractionDigits(1);
-        textField = new JFormattedTextField(format);
-        textField.setColumns(columns);
-        textField.setValue(initialValue);
+        textField = new JTextField(columns);
+        textField.setDocument(new PositiveDoubleDocument());
+        textField.setText(String.valueOf(initialValue));
     }
 
     @Override
     public Component getComponent() {
         return textField;
+    }
+    
+    private class PositiveDoubleDocument extends PlainDocument {
+        private static final long serialVersionUID = 2613991738327661701L;
+        
+        public void insertString(int offset, String s, AttributeSet attributeSet) throws BadLocationException {
+            try {
+                String value = textField.getText() + s;
+                Double.parseDouble(value);
+            } catch (NumberFormatException e) {
+                return;
+            }
+            super.insertString(offset, s, attributeSet);
+        }
+
     }
 
 }
